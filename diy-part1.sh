d57bd9941b1ca977aae6dea97f4a4ab5f315339a
mslovecc <33970221+mslovecc@users.noreply.github.com>
2024-01-15 08:27:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@ git fetch --tags
 git tag -l
  
 # Use OpenWrt 19.07.7 release
-git checkout v21.02.7
+git checkout v23.05.01
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 git clone https://github.com/mslovecc/luci-theme-argon.git package/luci-theme-argon
